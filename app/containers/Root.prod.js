37144502a834d9bfd94fd8b0f96207f44d827ff6
Jordan Burgess <jordanburgess@gmail.com>
2017-03-01 23:56:54 +0000

    feat: add GA tracking

--- app/containers/Root.prod.js
+++ app/containers/Root.prod.js
@@ -2,13 +2,20 @@ import React, {Component, PropTypes} from 'react';
 import {Provider} from 'react-redux';
 import routes from '../routes';
 import {Router} from 'react-router';
+var ReactGA = require('react-ga');
+ReactGA.initialize('UA-166982-13');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 export default class Root extends Component {
   render() {
     const {store, history} = this.props;
     return (
       <Provider store={store}>
-        <Router history={history} routes={routes}/>
+        <Router history={history} routes={routes} onUpdate={logPageView} />
       </Provider>
     );
   }
